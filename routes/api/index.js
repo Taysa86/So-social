3db9226ca4586692e0ba99247ac09efb907269b9
Taysa86 <Taysarolu86@gmail.com>
2023-03-30 13:00:18 -0500

    removed debugging code in preperation for turn in

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,23 +2,8 @@ const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const thoughtRoutes = require("./thoughtRoutes");
 
-/*const getAllUsers = require("./userRoutes")
-const createNewUser = require("./userRoutes")*/
-
 router.use("/users", userRoutes);
 router.use("/thoughts", thoughtRoutes);
-
-/*router.route("/user")
-  .get(function (req, res, next) {
-    console.log("GET request called");
-    getAllUsers(req);
-    res.end();
-  })
-  .post(function (req, res, next) {
-    console.log("POST request called");
-    createNewUser(req); 
-    res.end();
-  });*/
 
 
 module.exports = router;
